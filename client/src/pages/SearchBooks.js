bd6fd32884fa233b584506df67aaa9b2529abdb7
kelliekumasaka <kelliek3@gmail.com>
2021-12-15 15:10:02 -0800

    user-controller deletion

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -14,6 +14,8 @@ const SearchBooks = () => {
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+  // pass the mutation to useMutation
+  const [saveNewBook, { data, loading, error }] = useMutation(SAVE_BOOK)
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
@@ -66,7 +68,7 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      const response = await saveNewBook(bookToSave, token);
 
       if (!response.ok) {
         throw new Error('something went wrong!');
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,61 +1,61 @@
 import { gql } from "@apollo/client";
 
 export const LOGIN_USER = gql`
-    mutation login($email:String!, $password:String!){
-        login(email:$email, password:$password){
-            token
-            user{
-                _id
-                username
-            }
-        }
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
     }
-`
+  }
+`;
 
 export const ADD_USER = gql`
-    mutation addUser($email:String!, $password:String!, $username:String!){
-        addUser(email:$email,password:$password,username:$username){
-            token
-            user{
-                _id
-                username
-            }
-        }
+  mutation addUser($email: String!, $password: String!, $username: String!) {
+    createUser(email: $email, password: $password, username: $username) {
+      token
+      user {
+        _id
+        username
+      }
     }
-`
+  }
+`;
 
 export const SAVE_BOOK = gql`
-    mutation saveBook($bookData:svBk!){
-        saveBook(bookData:$bookData){
-            _id
-            username
-            email
-            savedBooks{
-                authors
-                title
-                bookId
-                description
-                image
-                link
-            }
-        }
+  mutation saveBook($bookData: svBk!) {
+    saveBook(bookData: $bookData) {
+      _id
+      username
+      email
+      savedBooks {
+        authors
+        title
+        bookId
+        description
+        image
+        link
+      }
     }
-`
+  }
+`;
 
 export const REMOVE_BOOK = gql`
-    mutation removeBook($bookId:ID!){
-        removeBook(bookId:$bookId){
-            _id
-            username
-            email
-            savedBooks{
-                authors
-                title
-                bookId
-                description
-                image
-                link
-            }
-        }
+  mutation removeBook($bookId: ID!) {
+    deleteBook(bookId: $bookId) {
+      _id
+      username
+      email
+      savedBooks {
+        authors
+        title
+        bookId
+        description
+        image
+        link
+      }
     }
-`
+  }
+`;
